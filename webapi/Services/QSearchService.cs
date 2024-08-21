87c57e91cae366c8ae0381cdd1159d14b7dcf51b
sam <samgoyal0044@gmail.com>
2024-08-21 12:26:30 -0700

    normalized common azure configs

--- webapi/Services/QSearchService.cs
+++ webapi/Services/QSearchService.cs
@@ -97,14 +97,14 @@ public class QSearchService : IQSearchService
     private string GetApiKey(string specializationKey)
     {
 #pragma warning disable CS8602 // Dereference of a possibly null reference.
-        return this._qAzureOpenAIChatExtension.GetSpecializationIndexByKey(specializationKey).APIKey;
+        return this._qAzureOpenAIChatExtension.GetAzureConfig().APIKey;
 #pragma warning restore CS8602 // Dereference of a possibly null reference.
     }
 
     private string GetEndpoint(string specializationKey)
     {
 #pragma warning disable CS8602 // Dereference of a possibly null reference.
-        return this._qAzureOpenAIChatExtension.GetSpecializationIndexByKey(specializationKey).Endpoint.ToString();
+        return this._qAzureOpenAIChatExtension.GetAzureConfig().Endpoint.ToString();
 #pragma warning restore CS8602 // Dereference of a possibly null reference.
     }
 
