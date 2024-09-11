d70f3978197f2220c5f24ac1ab2b13b36c204d86
JustinMacaulay <justinkmacaulay@gmail.com>
2024-09-11 10:04:45 -0700

    backend auth

---
+++ webapi/Services/UserMiddleware.cs
@@ -0,0 +1,62 @@
+// Copyright (c) Microsoft. All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Security.Principal;
+using CopilotChat.WebApi.Controllers;
+using CopilotChat.WebApi.Hubs;
+using CopilotChat.WebApi.Options;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.SemanticKernel;
+
+namespace CopilotChat.WebApi.Services;
+
+/// <summary>
+/// Middleware for determining is site is undergoing maintenance.
+/// </summary>
+public class UserMiddleware
+{
+    private readonly RequestDelegate _next;
+    // private readonly UserPermissions permissions;
+    private readonly ILogger<UserMiddleware> _logger;
+
+    public UserMiddleware(
+        RequestDelegate next,
+        ILogger<UserMiddleware> logger)
+
+    {
+        this._next = next;
+        this._logger = logger;
+    }
+
+    public async Task Invoke(HttpContext ctx, Kernel kernel)
+    {
+        // List<string> groups = await this.GetGroupsAsync(ctx.User.Identity?.Name);
+        // this._logger.LogInformation(groups?.ForEach(Console.WriteLine));
+
+        await this._next(ctx);
+    }
+
+
+    // private async Task<List<string>> GetGroupsAsync(string userName)
+    // {
+    //     List<string> result = new List<string>();
+    //     WindowsIdentity wi = new WindowsIdentity(userName);
+
+    //     foreach (IdentityReference group in wi.Groups)
+    //     {
+    //         try
+    //         {
+    //             result.Add(group.Translate(typeof(NTAccount)).ToString());
+    //         }
+    //         catch (Exception ex) { }
+    //     }
+    //     result.Sort();
+    //     return result;
+    // }
+
+}
