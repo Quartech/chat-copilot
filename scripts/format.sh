7a5c7c32c642bd45cb6445f2674be0850b038553
Mac Deluca <Mac.Deluca@quartech.com>
2024-09-13 15:32:17 -0700

    feat: updated csharpier format script + github action

--- scripts/format.sh
+++ scripts/format.sh
@@ -1,4 +1,24 @@
 #!/usr/bin/env bash
 
-# Run the formatter from the root directory on webapi/
-cd ../ && dotnet csharpier --check webapi
+##########################################################################
+#                           Csharpier Formatter
+#
+# How to use: ./format.sh [options]
+# Options: https://csharpier.com/docs/CLI
+#
+# Format (writes): ./format.sh
+# Check (reads): ./format.sh --check
+#
+# Note: By default, this script will format the entire webapi project.
+#
+# Note: This script will run from the root of the repository
+# where the .editorconfig file is located. This is to prevent conflicts
+# with dotnet-format which acts mostly as a linter and uses the same config.
+#
+# See: https://csharpier.com/docs/About
+#
+##########################################################################
+
+cd ..
+dotnet tool restore --tool-manifest webapi/.config/dotnet-tools.json
+dotnet csharpier "$@" webapi
