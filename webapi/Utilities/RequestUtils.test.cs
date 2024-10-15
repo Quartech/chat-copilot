93e5f4cfce01e5035112ba61c6a603ebce0a44e8
mattthiessendev <matt.thiessen.dev@gmail.com>
2024-10-15 14:34:44 -0700

    test: RequestUtils.GetSanitizedParameter

---
+++ webapi/Utilities/RequestUtils.test.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CopilotChat.WebApi.Utilities;
+
+[TestClass]
+public class RequestUtilsTest
+{
+    [TestMethod]
+    public void GetSanitizedParameter_SanitizeParameterString()
+    {
+        var expected = "ca138d45-9785-4ff3-9119-e77c5341ba95";
+        var parameterValue = $"ca138d45{Environment.NewLine}-9785-4ff3-9119-e77c5341ba95{Environment.NewLine}";
+
+        Assert.AreEqual(expected, RequestUtils.GetSanitizedParameter(parameterValue));
+    }
+}
+
