ad72a6bed91f187178f54962d969c7582ed26033
Graham Stewart <graham.stewart@quartech.com>
2024-10-22 11:36:48 -0700

    Added calc to height of search list to make sure results don't get pushed off page

--- webapp/src/components/search/search-list/SearchList.tsx
+++ webapp/src/components/search/search-list/SearchList.tsx
@@ -12,7 +12,7 @@ const useClasses = makeStyles({
         display: 'flex',
         flexShrink: 0,
         width: '320px',
-        height: '100%',
+        height: 'calc(100% - 44px)', //44px fixed height for tab list above this element.
         backgroundColor: tokens.colorNeutralBackground4,
         flexDirection: 'column',
         ...Breakpoints.small({
