8f8fd3bdaa6f35b680e0072d90d9976e2039455c
mattthiessendev <matt.thiessen.dev@gmail.com>
2024-11-14 14:51:21 -0800

    feat: create ContextValueAccessor

---
+++ webapi/Context/ContextValueAccessor.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace CopilotChat.WebApi.Context;
+
+public class ContextValueAccessor(IHttpContextAccessor contextAccessor) : IContextValueAccessor
+{
+    public object? GetRouteValue(string key)
+    {
+        var context = contextAccessor.HttpContext;
+
+        if (context == null)
+            return null;
+
+        context.Request.RouteValues.TryGetValue(key, out var routeValue);
+
+        return routeValue;
+    }
+}
