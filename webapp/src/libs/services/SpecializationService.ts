b15495e92b4bc53828eb3a07842c21acda7d1f74
hansonchun-quartech <hanson.chun@quartech.com>
2024-11-26 13:04:44 -0800

    Use null check instead of falsy check (#210)

--- webapp/src/libs/services/SpecializationService.ts
+++ webapp/src/libs/services/SpecializationService.ts
@@ -128,7 +128,7 @@ export class SpecializationService extends BaseService {
         formData.append('isDefault', body.isDefault.toString());
         formData.append('suggestions', JSON.stringify(body.suggestions));
         formData.append('canGenImages', body.canGenImages.toString());
-        if (body.restrictResultScope) {
+        if (body.restrictResultScope != null) {
             formData.append('restrictResultScope', body.restrictResultScope.toString());
         }
         if (body.strictness) {
