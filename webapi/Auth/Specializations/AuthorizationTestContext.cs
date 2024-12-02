5b87ac7b8ff20f9b6c4c7c96416fed7175184942
mattthiessendev <matt.thiessen.dev@gmail.com>
2024-12-02 15:52:46 -0800

    Specialization body authorization handler (#215)
    * feat: create AuthorizationHandler
    
    * refactor: pull BuildAuthorizationContext into helper
    
    * test: AuthorizationHandler
    
    * feat: create SpecializationBody
    
    * style: lint

---
+++ webapi/Auth/Specializations/AuthorizationTestContext.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+
+namespace CopilotChat.WebApi.Auth.Specializations;
+
+public static class AuthorizationTestContext
+{
+    public static AuthorizationHandlerContext BuildAuthorizationContext(IEnumerable<Claim> claims)
+    {
+        var requirements = new[] { new SpecializationRequirement() };
+
+        var user = new ClaimsPrincipal(new ClaimsIdentity(claims, "auth"));
+
+        return new(requirements, user, null);
+    }
+}
