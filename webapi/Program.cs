b9eaf0294c134ad0fe6f0db3633f0fc0827348bc
mattthiessendev <matt.thiessen.dev@gmail.com>
2024-12-13 15:03:24 -0800

    refactor: localize enum json serializer to CopilotChatMessageSortOption (#226)
    * fix: localize enum json serializer to CopilotChatMessageSortOption
    
    * style: lint

--- webapi/Program.cs
+++ webapi/Program.cs
@@ -4,7 +4,6 @@ using System;
 using System.Diagnostics;
 using System.Linq;
 using System.Text.Json;
-using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using CopilotChat.WebApi.Extensions;
 using CopilotChat.WebApi.Hubs;
@@ -94,7 +93,6 @@ public sealed class Program
             .AddJsonOptions(options =>
             {
                 options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
-                options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
             });
         builder.Services.AddHealthChecks();
 
